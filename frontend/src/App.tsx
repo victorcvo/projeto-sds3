1b38df90176a22ad0b8a6ba2c1f6415274224180
victorcvo <victorfape@gmail.com>
2021-11-09 07:51:10 -0300

    Static Grafico

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,35 @@
+import Barchart from "components/BarChart";
+import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
+import Footer from "components/Footer";
+import Navbar from "components/NavBar";
 
 
 function App() {
   return (
-    <div>
-      <h1>Olá Mundo</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de vendas</h1>
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text secondary">Pecenutal de vedas %</h5>
+            <Barchart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center tex-secondary">Todas as vendas</h5>
+            <DonutChart />
+          </div>
+        </div>
+          <div className="py-3">
+           <h2 className="text-primary">Todas as Vendas</h2>
+          </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
